2511b9d89dce5166420f9c529db0c9338d475b8c
Maros Betko <betkomaros@gmail.com>
2019-12-02 00:36:11 +0100

    Updates

---
+++ precache-manifest.ce2072cc562d88f71d67def09da2111d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04e4719c946fdd559cb5fc4f5231ecf",
+    "url": "/MTGAdventures/index.html"
+  },
+  {
+    "revision": "bc68d154d2289ef6d736",
+    "url": "/MTGAdventures/static/js/2.cce94ddf.chunk.js"
+  },
+  {
+    "revision": "32a83a5088ea6f6331ce",
+    "url": "/MTGAdventures/static/js/main.204d6c4c.chunk.js"
+  },
+  {
+    "revision": "cba5c053dccde4960393",
+    "url": "/MTGAdventures/static/js/runtime-main.6add0026.js"
+  }
+]);
